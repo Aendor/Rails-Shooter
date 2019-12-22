abba8de0cac9a911c4165429259f45cc75903db4
Aendor <arnoutcschouten@hotmail.com>
2019-12-22 14:33:43 +0100

    Added Waypoint System

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using u = UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 10f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
