af544577b8427342cb6aa6bffa0900d2d114ee6c
Aendor <arnoutcschouten@hotmail.com>
2019-12-22 18:02:07 +0100

    Added ship x y movement

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float speed = 12f;
+    [SerializeField] float xRange = 6f;
+    [SerializeField] float yRange = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float newRawXPos = transform.localPosition.x + xOffset;
+        float newRawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(newRawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(newRawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
+    }
+}
