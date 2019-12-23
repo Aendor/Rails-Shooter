682b789e2ae79f67ce9400f98a1b3544608f3cb6
Aendor <arnoutcschouten@hotmail.com>
2019-12-23 12:42:50 +0100

    Added ship rotational movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -9,17 +10,45 @@ public class Player : MonoBehaviour
     [SerializeField] float xRange = 6f;
     [SerializeField] float yRange = 4f;
 
+    [SerializeField] float positionPitchFactor = -9f;
+    [SerializeField] float controlPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 8f;
+    [SerializeField] float controlRollFactor = -40f;
+
+    float xThrow, yThrow;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yaw = yawDueToPosition;
+
+        float rollDueToControlThrow = xThrow * controlRollFactor;
+        float roll = rollDueToControlThrow;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * speed * Time.deltaTime;
         float yOffset = yThrow * speed * Time.deltaTime;
@@ -31,6 +60,5 @@ public class Player : MonoBehaviour
         float clampedYPos = Mathf.Clamp(newRawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-
     }
 }
